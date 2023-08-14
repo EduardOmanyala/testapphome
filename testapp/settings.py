46f2cdeddf69e6c1c33aaf7c03b5c94d8fe3674a
EduardOmanyala <bestessays001@gmail.com>
2023-08-14 22:40:19 +0300

    added new hosts

--- testapp/settings.py
+++ testapp/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testprepken.com','www.testprepken.com']
+#ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -79,24 +80,24 @@ WSGI_APPLICATION = 'testapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 #DATABASES = {
     #'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': config(DATABASENAME),
-        #'USER': config(DATABASEUSER),
-        #'PASSWORD': config(DATABASEPASSWORD),
-        #'HOST': 'localhost',
-        #'PORT': '',
-   # }
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 #}
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASENAME'),
+        'USER': config('DATABASEUSER'),
+        'PASSWORD': config('DATABASEPASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
